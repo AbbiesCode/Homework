25d0096e0197cf4d440dee6490a9d8a055918948
AbbiesCode <abbishannon@hotmail.co.uk>
2025-05-31 12:45:26 +0100

    Renamed: Number guess 1 to 6 in C++.cpp

--- Number guess 1 to 6 in C++.cpp
+++ Number guess 1 to 6 in C++.cpp
@@ -10,9 +10,7 @@ int randomInteger()
 	// Computer generates random number between 1-6 (found online)
 	srand((unsigned)time(NULL));
 	int random = (1 + rand() % 6);
-
-	// When I entirely guessed this part I wasn't far off! Originally I wrote: int match{ random == guess() }; and returned 'match'
-	// I later used the internet to help me.
+	
 	if (guess == random) std::cout << "Success you have guessed it!";
 
 	return 0;
@@ -21,7 +19,6 @@ int randomInteger()
 int main()
 {
 	randomInteger();
-	// I feel ike this should go on repeat so the user can input a number indefinitely? And a fail message for feedback too. 
 
 	return 0;
 }
